49df20a2c818f99248148c3a7e3f02acd1c9ce7d
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-09-26 22:53:44 +0300

    main character movements

--- iosGame/GameScene.swift
+++ iosGame/GameScene.swift
@@ -4,7 +4,7 @@
 //
 //  Created by IŞIL VARDARLI on 24.09.2024.
 //
-
+import UIKit
 import SpriteKit
 import GameplayKit
 
@@ -15,12 +15,18 @@ class GameScene: SKScene {
     var kirmiziUcgen:SKSpriteNode = SKSpriteNode()
     var siyahKare:SKSpriteNode = SKSpriteNode()
     var skorLabel:SKLabelNode = SKLabelNode()
-    
+    //for connect to GameViewController
     var viewController : UIViewController?
     
+    //controlling touching moves
+    var touchingControl = false
+    var timer: Timer?
+    
+    //beginning at the scene
     override func didMove(to view: SKView) {
         if let tempAnaKarekter = self.childNode(withName: "anakarekter") as? SKSpriteNode {
             anaKarekter = tempAnaKarekter
+            anaKarekter.isUserInteractionEnabled = true
         }
         if let tempSariDaire = self.childNode(withName: "sariDaire") as? SKSpriteNode {
             sariDaire = tempSariDaire
@@ -35,11 +41,25 @@ class GameScene: SKScene {
             skorLabel = tempSkorLabel
             skorLabel.text = "Skor: 0"
         }
+        timer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector (GameScene.hareketEttir), userInfo: nil, repeats: true)
     }
     
+    @objc func hareketEttir() {
+        
+        if touchingControl {
+            print("Hareket Ettir çalıştı: touchingControl: \(touchingControl)")
+            let yukariHareket: SKAction = SKAction.moveBy(x: 0, y: +20, duration: 1)
+            anaKarekter.run(yukariHareket)
+        } else {
+            let asagiHareket: SKAction = SKAction.moveBy(x: 0, y: -20, duration: 1)
+            anaKarekter.run(asagiHareket)
+        }
+    }
+
     
     func touchDown(atPoint pos : CGPoint) {
-        
+        touchingControl = true
+        print("ekrana dokundu, touchingControl: \(touchingControl)")
     }
     
     func touchMoved(toPoint pos : CGPoint) {
@@ -47,10 +67,17 @@ class GameScene: SKScene {
     }
     
     func touchUp(atPoint pos : CGPoint) {
-
+        touchingControl = false
+        print("ekranı bıraktı, touchingControl: \(touchingControl)")
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Touch began")
+        if let touch = touches.first {
+            let position = touch.location(in: self)
+            touchDown(atPoint: position)
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -58,7 +85,11 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.viewController?.performSegue(withIdentifier: "gameToSkor", sender: nil)
+        //self.viewController?.performSegue(withIdentifier: "gameToSkor", sender: nil)
+        if let touch = touches.first {
+            let position = touch.location(in: self)
+            touchUp(atPoint: position)
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
